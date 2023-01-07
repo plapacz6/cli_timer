afc637635e982d8fef5e56f2cc1b668b7e55bd71
pawel <plapacz6@gmail.com>
2023-01-07 23:32:33 +0100

    correct and exapnding tests of add_tm2tm

--- tests/test_add_tm2nowtm.cpp
+++ tests/test_add_tm2nowtm.cpp
@@ -3,35 +3,48 @@
 #include "../add_tm2nowtm.h"
 #include <time.h>
 
-
-TEST_CASE("adding seconds", "[seconds]"){
+TEST_CASE("adding amount of time to struct tm <time.h>") {
   struct tm tm0;
   struct tm tm1;
-  struct tm tm2;
+  struct tmA tm2;
   struct tm tm3;
 
+SECTION("adding seconds", "[seconds]"){
   time_t now;
   time(&now);
   tm0 = *localtime(&now);  //copy of interenal tm struct
 
   tm1 = tm0;
-  tm2 = tm0;
-
+  
+  //amounth of time
+  //calcualted
   tm2.tm_isdst = 0;
-  tm2.tm_yday = 1;
-  tm2.tm_wday = 1;
-  tm2.tm_year = 1;
-  tm2.tm_mon = 1;
-  tm2.tm_mday = 1;
+  tm2.tm_yday = 0;
+  tm2.tm_wday = 0;
+  //editable
+  tm2.tm_year = 0; //123
+  tm2.tm_mon = 0;
+  tm2.tm_mday = 0;
   tm2.tm_hour = 0;
   tm2.tm_min = 0;
   tm2.tm_sec = 0;
 
-///////////////////////////////// TO CORRRECT  vvvv            /////////////////////////////////
-  SECTION(""){
+  SECTION("all pozition at last pozistion"){
+    //TODO - and adding 1 sec
+  }
+
+  SECTION("minute on beginnin"){
     tm1.tm_min = 1;
-    tm2.tm_min = 1;
-    SECTION("borderline case 0+1"){
+    
+    SECTION("adding zero sec 0"){
+      tm1.tm_sec = 0;
+      tm2.tm_sec = 0;
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_sec == 0);
+      REQUIRE(tm3.tm_min == tm2.tm_min);      
+    };        
+    SECTION("adding 1 sec do 0"){
       tm1.tm_sec = 0;
       tm2.tm_sec = 1;
 
@@ -39,7 +52,7 @@ TEST_CASE("adding seconds", "[seconds]"){
       REQUIRE(tm3.tm_sec == 1);
       REQUIRE(tm3.tm_min == tm2.tm_min);
     };
-    SECTION("borderline case 59+1"){
+    SECTION("adding 1 sec to 59"){
       tm1.tm_sec = 59;
       tm2.tm_sec = 1;
 
@@ -47,44 +60,32 @@ TEST_CASE("adding seconds", "[seconds]"){
       REQUIRE(tm3.tm_sec == 0);
       REQUIRE(tm3.tm_min == tm2.tm_min + 1);      
     };
-    SECTION("borderline case 0+0"){
-      tm1.tm_sec = 0;
-      tm2.tm_sec = 0;
+    SECTION("adding 59 sec to 1"){
+      tm1.tm_sec = 1;
+      tm2.tm_sec = 59;
 
       tm3 = add_tm2tm(tm1, tm2);
       REQUIRE(tm3.tm_sec == 0);
-      REQUIRE(tm3.tm_min == tm2.tm_min);      
-    };    
-  };
-  
+      REQUIRE(tm3.tm_min == tm2.tm_min + 1);      
+    };
+  };  
 };
-TEST_CASE("adding minuntes", "[minuntes]"){
-  struct tm tm0;
-  struct tm tm1;
-  struct tm tm2;
-  struct tm tm3;
 
-  time_t now;
-  time(&now);
-  tm0 = *localtime(&now);  //copy of interenal tm struct
 
-  tm1 = tm0;
-  tm2 = tm0;
+SECTION("adding minuntes", "[minuntes]"){
 
-  tm2.tm_isdst = 0;
-  tm2.tm_yday = 1;
-  tm2.tm_wday = 1;
-  tm2.tm_year = 1;
-  tm2.tm_mon = 1;
-  tm2.tm_mday = 1;
-  tm2.tm_hour = 0;
-  tm2.tm_min = 0;
-  tm2.tm_sec = 0;
-
-  SECTION(""){
+  SECTION("hours on begining"){
     tm1.tm_hour = 1;
-    tm2.tm_hour = 1;
-    SECTION("borderline case 0+1"){
+
+    SECTION("minute 0 + 0"){
+      tm1.tm_min = 0;
+      tm2.tm_min = 0;
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_min == 0);
+      REQUIRE(tm3.tm_hour == tm2.tm_hour);
+    };     
+    SECTION("minute 0 + one minute"){
       tm1.tm_min = 0;
       tm2.tm_min = 1;
 
@@ -92,7 +93,7 @@ TEST_CASE("adding minuntes", "[minuntes]"){
       REQUIRE(tm3.tm_min == 1);
       REQUIRE(tm3.tm_hour == tm2.tm_hour);
     };
-    SECTION("borderline case 59+1"){
+    SECTION("minute 59 + one minunte"){
       tm1.tm_min = 59;
       tm2.tm_min = 1;
 
@@ -100,166 +101,201 @@ TEST_CASE("adding minuntes", "[minuntes]"){
       REQUIRE(tm3.tm_min == 0);
       REQUIRE(tm3.tm_hour == tm2.tm_hour + 1);
     };
-    SECTION("borderline case 0+0"){
-      tm1.tm_min = 0;
-      tm2.tm_min = 0;
+    SECTION("minute 1 + 59 minuntes"){
+      tm1.tm_min = 1;
+      tm2.tm_min = 59;
 
       tm3 = add_tm2tm(tm1, tm2);
       REQUIRE(tm3.tm_min == 0);
-      REQUIRE(tm3.tm_hour == tm2.tm_hour);
-    };        
+      REQUIRE(tm3.tm_hour == tm2.tm_hour + 1);
+    };   
   };
 };  
 
-TEST_CASE("adding hours", "[hours]"){
-  struct tm tm0;
-  struct tm tm1;
-  struct tm tm2;
-  struct tm tm3;
 
-  time_t now;
-  time(&now);
-  tm0 = *localtime(&now);  //copy of interenal tm struct
-
-  tm1 = tm0;
-  tm2 = tm0;
-
-  tm2.tm_isdst = 0;
-  tm2.tm_yday = 1;
-  tm2.tm_wday = 1;
-  tm2.tm_year = 1;
-  tm2.tm_mon = 1;
-  tm2.tm_mday = 1;
-  tm2.tm_hour = 0;
-  tm2.tm_min = 0;
-  tm2.tm_sec = 0;
+SECTION("adding hours", "[hours]"){
 
   SECTION(""){
     tm1.tm_mday = 1;
-    tm2.tm_mday = 1;
-    SECTION("borderline case 0+1"){
-      tm1.tm_hour = 0;
-      tm2.tm_hour = 1;
-
-      tm3 = add_tm2tm(tm1, tm2);
-      REQUIRE(tm3.tm_hour == 1);
-      REQUIRE(tm3.tm_mday == tm2.tm_mday);
-    };
-    SECTION("borderline case 23+1"){      
-      tm1.tm_hour = 23;
-      tm2.tm_hour = 1;
-
-      tm3 = add_tm2tm(tm1, tm2);
-      REQUIRE(tm3.tm_hour == 0);
-      REQUIRE(tm3.tm_mday == tm2.tm_mday + 1);
-    };
-    SECTION("borderline case 0+0"){
+    
+    SECTION("hour 0 + 0"){
       tm1.tm_hour = 0;
       tm2.tm_hour = 0;
 
       tm3 = add_tm2tm(tm1, tm2);
       REQUIRE(tm3.tm_hour == 0);
-      REQUIRE(tm3.tm_mday == tm2.tm_mday);
-    };        
+      REQUIRE(tm3.tm_mday == 1);
+    };            
+    SECTION("hour 0+ one hour"){
+      tm1.tm_hour = 0;
+      tm2.tm_hour = 1;
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_hour == 1);
+      REQUIRE(tm3.tm_mday == 1);
+    };
+    SECTION("hour 23 + one hour"){      
+      tm1.tm_hour = 23;
+      tm2.tm_hour = 1;
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_hour == 0);
+      REQUIRE(tm3.tm_mday == 2);
+    };
+    SECTION("hour 0 + 23 hours"){      
+      tm1.tm_hour = 1;
+      tm2.tm_hour = 23;
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_hour == 0);
+      REQUIRE(tm3.tm_mday == 2);
+    };
   };
 };
 
-TEST_CASE("adding days", "[days day_of_week day_of_year]"){
-  struct tm tm0;
-  struct tm tm1;
-  struct tm tm2;
-  struct tm tm3;
 
-  time_t now;
-  time(&now);
-  tm0 = *localtime(&now);  //copy of interenal tm struct
+SECTION("adding days", "[days day_of_week day_of_year]"){
 
-  tm1 = tm0;
-  tm2 = tm0;
+  SECTION(""){    
+    tm1.tm_mon = 1; //January    
 
-  tm2.tm_isdst = 0;
-  tm2.tm_yday = 1;
-  tm2.tm_wday = 1;
-  tm2.tm_year = 1;
-  tm2.tm_mon = 1;
-  tm2.tm_mday = 1;
-  tm2.tm_hour = 0;
-  tm2.tm_min = 0;
-  tm2.tm_sec = 0;
+    SECTION("first day of month +0 days"){
+      tm1.tm_mday = 1;  //
+      tm1.tm_yday = 0; 
+      tm1.tm_wday = 1;  //monday
 
-  SECTION(""){
-    tm1.tm_mon = 1;
-    tm2.tm_mon = 1;
-    SECTION("borderline case 0+1"){
-      tm1.tm_mday = 0;
-      tm2.tm_mday = 1;
-      tm1.tm_yday = 1;      
-//////////////////////////////////////////////////////////////////
+      tm2.tm_mday = 0;  //we adding 0-days 
+
       tm3 = add_tm2tm(tm1, tm2);
-      REQUIRE(tm3.tm_mday == 1);
-      REQUIRE(tm3.tm_mon == tm2.tm_mon);
-      REQUIRE(tm3.tm_yday == tm2.tm_yday);
-      REQUIRE(tm3.tm_wday == tm2.tm_wday);      
+      REQUIRE(tm3.tm_mday == 0); //w/o change
+      REQUIRE(tm3.tm_mon == 0);  //w/o change
+      //we adding 0 days to 1 day of year      
+      REQUIRE(tm3.tm_yday == 0); //w/o change
+      REQUIRE(tm3.tm_wday == 1); //w/o change : monday
+    };            
+    SECTION("day 0 (1-st of month) + one day"){
+      tm1.tm_mday = 1;  //first day of month (31-days January)
+      tm1.tm_yday = 0;  //first day of yaer    
+      tm1.tm_wday = 1;  //first day of week (monday)
+
+      tm2.tm_mday = 1;  //we adding one day
+
+      tm3 = add_tm2tm(tm1, tm2);      
+      REQUIRE(tm3.tm_mday == 1); 
+      REQUIRE(tm3.tm_mon == 0);  //January remain      
+      REQUIRE(tm3.tm_yday == 1); 
+      REQUIRE(tm3.tm_wday == 2); 
     };
-    SECTION("borderline case 31+1"){      
-      tm1.tm_mday = 31;
-      tm2.tm_mday = 1;
+    SECTION("day 1 + 31 days"){      
+      tm1.tm_mday = 1; //first day of month
+      tm1.tm_yday = 0; 
+      tm1.tm_wday = 1;  //monday
+
+      tm2.tm_mday = 31;  //we adding all day of current month
 
       tm3 = add_tm2tm(tm1, tm2);
-      REQUIRE(tm3.tm_mday == 1);
-      REQUIRE(tm3.tm_mon == tm2.tm_mon + 1);
-      REQUIRE(tm3.tm_yday == tm2.tm_yday + 1);      
+      REQUIRE(tm3.tm_mday == 1); //first day of next month
+      REQUIRE(tm3.tm_mon == 2);  //February
+      REQUIRE(tm3.tm_yday == 31); //31 of January + 
+      REQUIRE(tm3.tm_wday == tm1.tm_wday + 31 % 7); //tuesday - one day after
+    };    
+    SECTION("january day 31+ one day"){      
+      tm1.tm_mday = 31; //last day of month
+      tm1.tm_yday = 30; 
+      tm1.tm_wday = 1;  //monday
+
+      tm2.tm_mday = 1;  //we adding one day
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_mday == 1); //first day of next month
+      REQUIRE(tm3.tm_mon == 2);  //February
+      REQUIRE(tm3.tm_yday == 31); //31 of January + 
+      REQUIRE(tm3.tm_wday == tm1.tm_wday + 31 % 7); //tuesday - one day after
+    };    
+    SECTION("non-transitional february day 28+ one day"){     
+      tm1.tm_mon = 2; //February
+      tm2.tm_isdst = 0; //28days
+
+      tm1.tm_year = 1983;
+      tm1.tm_mday = 31+28; //last day of month
+      tm1.tm_yday = 30+28-1; 
+      tm1.tm_wday = 1;  //monday
+
+      tm2.tm_mday = 1;  //we adding one day
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_mday == 1); //first day of next month
+      REQUIRE(tm3.tm_mon == 3); 
+      REQUIRE(tm3.tm_yday == 0+31+28); 
+      REQUIRE(tm3.tm_wday == tm1.tm_wday + 28 % 7); 
+      REQUIRE(tm3.tm_isdst == 0); 
     };
-    SECTION("borderline case 0+0"){
-      tm1.tm_mday = 0;
-      tm2.tm_mday = 0;
+    SECTION("transitional february day 28+ one day"){     
+      tm1.tm_mon = 2; //February
+      tm2.tm_isdst = 1; //29days
+
+      tm1.tm_year = 1984;
+      tm1.tm_mday = 31+28; //last day of month
+      tm1.tm_yday = 0+31+28-1; 
+      tm1.tm_wday = 1;  //monday
+
+      tm2.tm_mday = 1;  //we adding one day
 
       tm3 = add_tm2tm(tm1, tm2);
-      REQUIRE(tm3.tm_mday == 0);
-      REQUIRE(tm3.tm_mon == tm2.tm_mon);
-      REQUIRE(tm3.tm_yday == tm2.tm_yday);      
-    };        
+      REQUIRE(tm3.tm_mday == 29); //first day of next month
+      REQUIRE(tm3.tm_mon == 2); 
+      REQUIRE(tm3.tm_yday == 0+31+28); 
+      REQUIRE(tm3.tm_wday == tm1.tm_wday + 28 % 7); 
+      REQUIRE(tm3.tm_isdst == 1); 
+    };    
+    SECTION("transitional february day 29+ one day"){     
+      tm1.tm_mon = 2; //February
+      tm2.tm_isdst = 1; //29days      
+
+      tm1.tm_year = 1984;
+      tm1.tm_mday = 29; //last day of month
+      tm1.tm_yday = 0+31+29-1; 
+      tm1.tm_wday = 1;  //monday
+
+      tm2.tm_mday = 1;  //we adding one day
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_mday == 1); //first day of next month
+      REQUIRE(tm3.tm_mon == 3);  
+      REQUIRE(tm3.tm_yday == 0+31+29); 
+      REQUIRE(tm3.tm_wday == tm1.tm_wday + 29 % 7); 
+      REQUIRE(tm3.tm_isdst == 1); 
+    };
+    SECTION("march day 30+ one day"){     
+      tm1.tm_mon = 3; //March
+
+      tm1.tm_mday = 30; //last day of month
+      tm1.tm_yday = 0+31+28+30-1; 
+      tm1.tm_wday = 1;  //monday
+
+      tm2.tm_mday = 1;  //we adding one day
+
+      tm3 = add_tm2tm(tm1, tm2);
+      REQUIRE(tm3.tm_mday == 1); //first day of next month
+      REQUIRE(tm3.tm_mon == 4);  
+      REQUIRE(tm3.tm_yday == 0+31+28+30); //31 of January + 
+      REQUIRE(tm3.tm_wday == tm1.tm_wday + 30 % 7); //tuesday - one day after
+    };    
+   
+
+
   };
 };
 
-///////////////////////////////// TO CORRRECT   ^^^^^^^^^            /////////////////////////////////
-TEST_CASE("adding month", "[month]"){
+
+SECTION("adding month", "[month]"){
 
 };
-TEST_CASE("adding yeasr", "[years dist_year]"){
+
+
+SECTION("adding yeasr", "[years dist_year]"){
+
 };
 
+}; //TEST CASE
 
-
-  // tm1.tm_isdst =
-  // tm1.tm_yday = 
-  // tm1.tm_wday = 
-  // tm1.tm_year = 
-  // tm1.tm_mon = 
-  // tm1.tm_mday = 
-  // tm1.tm_hour = 
-  // tm1.tm_min = 
-  // tm1.tm_sec = 
-
-  // tm2.tm_isdst = 
-  // tm2.tm_yday = 
-  // tm2.tm_wday = 
-  // tm2.tm_year = 
-  // tm2.tm_mon = 
-  // tm2.tm_mday = 
-  // tm2.tm_hour = 
-  // tm2.tm_min = 
-  // tm2.tm_sec = 
-
-  // tm3.tm_isdst = tm1.tm_isdst;
-  // tm3.tm_yday = tm1.tm_yday;
-  // tm3.tm_wday = tm1.tm_wday;
-  // tm3.tm_year = tm1.tm_year;
-  // tm3.tm_mon = tm1.tm_mon;
-  // tm3.tm_mday = tm1.tm_mday;
-  // tm3.tm_hour = tm1.tm_hour;
-  // tm3.tm_min = tm1.tm_min;
-  // tm3.tm_sec = tm1.tm_sec;
-
-  //struct tm add_tm2nowtm(const struct tm tm1);
-  //struct tm add_tm2tm(const struct tm tm1, const struct tm tm2);
